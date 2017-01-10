814f639176a13436ee3745386ebf20860170572c
GallupinGallup <gallupingallup@gmail.com>
2017-01-10 13:57:32 -0700

    Made listeners

---
+++ src/controller/PongController.java
@@ -0,0 +1,8 @@
+package controller;
+
+public class PongController {
+	public void start()
+	{
+		
+	}
+}
---
+++ src/controller/PongRunner.java
@@ -0,0 +1,12 @@
+package controller;
+
+import cookie.controller.CookieController;
+
+public class PongRunner 
+{
+	public static void main(String[]args)
+	{
+		PongController controller = new PongController();
+		controller.start();
+	}
+}
